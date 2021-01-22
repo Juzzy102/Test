5a2e878653d3d4316cedbed2a1c0545116cf9d9c
Juzzy102 <justin.timora@gmail.com>
2021-01-22 16:11:34 +1100

    First Commit

--- test.R
+++ test.R
@@ -1,1 +1,51 @@
-Testing
+# Selecting a file manually
+
+stats <- read.csv(file.choose())
+
+# Method = setting the working directory
+getwd()
+
+#Setting WD - 
+#setwd("C:\\Users\\xxxx")
+
+#------------ Creating data frames
+mydf <- data.frame(Countries_2012_Dataset, Codes_2012_Dataset, Regions_2012_Dataset)
+head(mydf)
+
+# Changing the column names
+#colnames(mydf) <-c("Country", "Code", "Region")
+#head(mydf)
+
+#Shortcut
+mydf <- data.frame(Country = Countries_2012_Dataset, Code = Codes_2012_Dataset, Region = Regions_2012_Dataset)
+head(mydf)
+
+
+#Merging data frames
+merged <- merge(stats, mydf, by.x = "Country.Code", by.y = "Code")
+head(merged)
+
+merged$Country <-NULL
+str(merged)
+head(merged)
+
+#Visualising Data 
+
+qplot(data = merged, x = Internet.users, y = Birth.rate)
+qplot(data = merged, x = Internet.users, y = Birth.rate,
+      colour = Region)
+qplot(data = merged, x = Internet.users, y = Birth.rate,
+      colour = Region, size = I(5), shape=I(2))
+
+# Transparency
+
+qplot(data = merged, x = Internet.users, y = Birth.rate,
+      colour = Region, size = I(5), shape=I(19), alpha = I(0.6) ) #alpha controls transparency
+
+#Title
+qplot(data = merged, x = Internet.users, y = Birth.rate,
+      colour = Region, size = I(5), shape=I(19), alpha = I(0.6),
+      main = "Birthrate vs Internet Users") 
+
+
+
